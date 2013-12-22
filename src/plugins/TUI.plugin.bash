17b171c60752db22e3cf36f36fc47bb26116cc98
root <root@debian>
2013-12-22 03:51:24 +0100

    Fixed

--- src/plugins/TUI.plugin.bash
+++ src/plugins/TUI.plugin.bash
@@ -5,7 +5,6 @@ load color screen_display
 
 
 
-declare -A menuopts menufuncs; 
 start_menu(){ echo_center "$1" "$2"; menu_vwall $2; }
 menu_failed_response(){ _ "Error, try again"; }
 menu_wall(){ colorize none,${theme['menu_separator_color']} ${theme['menu_separator']}; }
@@ -13,13 +12,15 @@ menu_vwall(){ colorize none,${theme['menu_separator_color']} "${theme['menu_edge
 menu_entry(){ tput sc; menu_wall; colorize none,${theme['menu_number']} "$2)"; echo -n " "; colorize none,${theme['normal']} "$1"; tput rc; screen_goto_col $3; echo $(menu_wall);}
 menu_get_response(){ read -p "`_ \"Enter option: \"`" response; (( $response > $1 )) && response=-127; }
 mkmenu(){
-    document "mkmenu" "Create a menu" "[-t title] [ -o options ] [ -f functions ]" && return 
-    while getopts "s:o:f:t:" opt; do 
-        case $opt in 
-            o) menuopts[${#menuopts[@]}]=$OPTARG;; 
-            f) menufuncs[${#menufuncs[@]}]=$OPTARG;; 
+    unset menuopts
+    declare -A menuopts menufuncs;
+    document "mkmenu" "Create a menu" "[-t title] [ -o options ] [ -f functions ]" && return
+    while getopts "s:o:f:t:" opt; do
+        case $opt in
+            o) menuopts[${#menuopts[@]}]=$OPTARG;;
+            f) menufuncs[${#menufuncs[@]}]=$OPTARG;;
             s) set_name=$OPTARG;;
-            t) title=$OPTARG;; 
+            t) title=$OPTARG;;
         esac;
     done
     status=0; menu_len=$(( $(max_len_in_array "${menuopts[@]}") + 5 ));(( $menu_len < ${#title} )) && menu_len=$((${#title} + 4 ));
@@ -35,6 +36,6 @@ mkmenu(){
                ${menufuncs[$(( $response - 1 ))]}; break ;
             }
         } || { menu_failed_response; }
-    done 
+    done
 }
 
