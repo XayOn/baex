d88afdf123139f889ee745be8150495116a0d85a
root <root@debian>
2013-12-22 05:31:03 +0100

    Fixed trouble when recreating menus. Adding an option to create menus from csv files directories

--- src/plugins/TUI.plugin.bash
+++ src/plugins/TUI.plugin.bash
@@ -3,7 +3,7 @@
 document_description "Nice menu creation function not depending on ncurses, just pure bash"
 load color screen_display
 
-
+declare -A menuopts menufuncs;
 
 start_menu(){ echo_center "$1" "$2"; menu_vwall $2; }
 menu_failed_response(){ _ "Error, try again"; }
@@ -11,9 +11,26 @@ menu_wall(){ colorize none,${theme['menu_separator_color']} ${theme['menu_separa
 menu_vwall(){ colorize none,${theme['menu_separator_color']} "${theme['menu_edge']}$(mkline "${theme['separator']}" $(($1 -2 )))${theme['menu_edge']}";echo; }
 menu_entry(){ tput sc; menu_wall; colorize none,${theme['menu_number']} "$2)"; echo -n " "; colorize none,${theme['normal']} "$1"; tput rc; screen_goto_col $3; echo $(menu_wall);}
 menu_get_response(){ read -p "`_ \"Enter option: \"`" response; (( $response > $1 )) && response=-127; }
+mkmenufromcsvdir(){
+    document "help" "Create a menu from a directory of csv files" "directory" && return
+    read_csv $1 main.csv
+    build_menu
+}
+
+read_csv() {
+    unset menuopts menufuncs
+    declare -gA menuopts menufuncs
+    while IFS=, read opt func; do
+        [[ -e $1/$opt ]] && read_csv $1 $opt || {
+            menuopts[${#menuopts[@]}]=$opt;
+            menufuncs[${#menufuncs[@]}]=$func;
+        }
+    done < $1/$2
+}
+
 mkmenu(){
-    unset menuopts
-    declare -A menuopts menufuncs;
+    unset menuopts menufuncs
+    declare -gA menuopts menufuncs
     document "mkmenu" "Create a menu" "[-t title] [ -o options ] [ -f functions ]" && return
     while getopts "s:o:f:t:" opt; do
         case $opt in
@@ -23,6 +40,11 @@ mkmenu(){
             t) title=$OPTARG;;
         esac;
     done
+    OPTIND=0
+    build_menu
+}
+
+build_menu() {
     status=0; menu_len=$(( $(max_len_in_array "${menuopts[@]}") + 5 ));(( $menu_len < ${#title} )) && menu_len=$((${#title} + 4 ));
     start_menu "$title" $menu_len
     for i in "${menuopts[@]}"; do addone status; menu_entry "${i[@]}" $status $menu_len; done
